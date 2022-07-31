e5235982eb6b99ef8c5f0b022a6c85602e31804f
Anthony Thomasson <athomasson.93@gmail.com>
2022-07-30 18:07:50 -0600

    - adding install

--- setup/utilities/vundle-install.sh
+++ setup/utilities/vundle-install.sh
@@ -1,8 +1,12 @@
-if [ ! -d "~/.vim/bundle/Vundle.vim" ]
+VUNDEL_INSTALL_DIR="$HOME/.vim/bundle/Vundle.vim"
+
+if [ ! -d "$VUNDEL_INSTALL_DIR" ]
 then
 	echo "Cloning Vundle..."
-	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	git clone https://github.com/VundleVim/Vundle.vim.git $VUNDEL_INSTALL_DIR
 	echo "DONE"
+else
+	echo "Vundle already installed"
 fi
 echo "Installing Modules..."
 vim +PluginInstall +qall
