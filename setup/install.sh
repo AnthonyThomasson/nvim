8705aba01fe850d7bb7073022cf8212ca0d195dc
Anthony Thomasson <athomasson@Anthonys-MacBook.local>
2023-09-16 20:30:38 -0600

    Add system link so you can clone the repo anywhere

--- setup/install.sh
+++ setup/install.sh
@@ -1,9 +1,11 @@
 MY_PATH=$(dirname "$0")
 
+ln -s $PWD $HOME/.config
+
 sh $MY_PATH/utilities/vundle-install.sh
 sh $MY_PATH/utilities/brew-installs.sh
 
 cd ~/.vim/bundle/coc.nvim
-yarn upgrade
-yarn install
-yarn build
+npm upgrade
+npm install
+npm run build
